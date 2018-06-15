dd0fcae5e071c7e4cc9c695536ece8c28a7a60ad
DirtyIndustry <wlhdgm@gmail.com>
2018-06-15 21:26:21 +0800

    a working xml parser

--- src/renderer/utils/xmlReader.js
+++ src/renderer/utils/xmlReader.js
@@ -1,6 +1,5 @@
 import fs from 'fs'
 import path from 'path'
-import xml2js from 'xml2js'
 
 export default {
     name: 'xmlReader',
@@ -45,28 +44,106 @@ export default {
         },
         readXml (filepath) {
             let xml = fs.readFileSync(filepath)
-            xml2js.parseString(
-                xml,
-                {
-                    explicitArray: true,
-                    explicitRoot: false,
-                    valueProcessors: [this.convertProcessor]
-                },
-                (err, obj) => {
-                    if (err) {
-                        return console.log(err)
-                    }
-                    this.$store.dispatch('addSpider',obj)
-                }
-            )
+            let result = this.Xml2Obj(xml)
+            this.$store.dispatch('addSpider', result)
         },
-        convertProcessor (value, name) {
-            if (name === 'active') {
-                if (value === 'true') return true
-                else return false
-            } else {
-                return value
+        Xml2Obj (xml) {
+            let result = {}
+            let xmlDoc = xml.toString()
+            if (xmlDoc.startsWith('<?')) {
+                xmlDoc = xmlDoc.split('?>')
+                xmlDoc = xmlDoc[xmlDoc.length - 1]
+                xmlDoc = this.trimString(xmlDoc)
             }
+            let endindex = xmlDoc.indexOf('>')
+            let rootname = xmlDoc.substring(1, endindex)
+            let startindex = ('<' + rootname + '>').length
+            endindex = xmlDoc.indexOf('</' + rootname + '>')
+            let root = xmlDoc.substring(startindex, endindex)
+            root = this.trimString(root)
+            let childnodes = this.GetChildNodes(root)
+            for(let childnode of childnodes){
+                if (childnode.childnodes.length > 0){ // is Array
+                    result[childnode.nodename] = []
+                    for(let grandchild of childnode.childnodes){
+                        let obj = {}
+                        for(let propnode of grandchild.childnodes){
+                            obj[propnode.nodename]=this.parseValue(propnode.nodebody)
+                        }
+                        result[childnode.nodename].push(obj)
+                    }
+                }else if (childnode.nodeprops.length > 0){ // is Object
+                    result[childnode.nodename] = this.Prop2Obj(childnode)
+                } else { // is Property
+                    result[childnode.nodename] = this.parseValue(childnode.nodebody)
+                }
+            }
+            return result
+        },
+        GetChildNodes(node) {
+            let childnodelist = []
+            let restNodeDoc = node
+            for (; restNodeDoc !== '';) {
+                let nodename = ''
+                let nodebody = ''
+                let props = []
+                let childnodes = []
+
+                let endindex = restNodeDoc.indexOf('>')
+                let nodehead = restNodeDoc.substring(1, endindex)
+                if(nodehead.includes(' ')){ // has props
+                    nodehead.trim()
+                    let nodeheadarray = nodehead.split(' ')
+                    nodename = nodeheadarray[0]
+                    for(let i = 1; i < nodeheadarray.length; i++){
+                        let proparray = nodeheadarray[i].split('=')
+                        let prop = { propname: proparray[0], propvalue: proparray[1].substring(1, proparray[1].length-1)}
+                        props.push(prop)
+                    }
+                } else { // dosen't have prop
+                    nodename = nodehead
+                }
+                let startindex = endindex + 1
+                endindex = restNodeDoc.indexOf('</' + nodename + '>')
+                if (endindex === -1){ // not a valid node
+                    break
+                }
+                nodebody = restNodeDoc.substring(startindex, endindex)
+                nodebody = this.trimString(nodebody)
+                childnodes = this.GetChildNodes(nodebody)
+                restNodeDoc = restNodeDoc.substring(endindex + nodename.length + 3)
+                restNodeDoc = this.trimString(restNodeDoc)
+                let nodeobj = { nodename: nodename, nodebody: nodebody, nodeprops: props, childnodes: childnodes }
+                childnodelist.push(nodeobj)
+            }
+            return childnodelist
+        },
+        Prop2Obj(node){
+            let result = {}
+            for(let prop of node.nodeprops){
+                result[prop.propname] = this.parseValue(prop.propvalue)
+            }
+            return result
+        },
+        trimString (arg) {
+            for (; arg.startsWith('\n');) {
+                arg = arg.substring(1)
+            }
+            for (; arg.endsWith('\n');) {
+                arg = arg.substring(0, arg.length - 1)
+            }
+            return arg.trim()
+        },
+        parseValue(value){
+            let result = value
+            if (result.toLowerCase() === 'true'){
+                result = true
+            }else if(result.toLowerCase() === 'false'){
+                result = false
+            }else if(!isNaN(result)){
+                result = parseInt(result)
+            }
+            return result
         },
     },
 }
--- src/renderer/utils/xmlWriter.js
+++ src/renderer/utils/xmlWriter.js
@@ -1,6 +1,5 @@
 import fs from 'fs'
 import path from 'path'
-import xml2js from 'xml2js'
 
 export function deleteXml (filename) {
     fs.unlink(path.join(__static, 'Entry Files', filename + '.xml'), err => {
@@ -12,10 +11,8 @@ export function deleteXml (filename) {
 
 export function writeXml (spider) {
     let filepath = path.join(__static, 'Entry Files', spider.name + '.xml')
-    let xmlValues = new xml2js.Builder({ rootname: 'spider' })
-    let xmlString = xmlValues.buildObject(spider)
 
-
+    let xmlString = Obj2Xml(spider)
 
     fs.writeFile(filepath, xmlString, err => {
         if (err) {
@@ -24,4 +21,34 @@ export function writeXml (spider) {
     })
 }
 
+function Obj2Xml(obj){
+    let result = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n'
+    result += Obj2Node(obj, 'root')
+    return result
+}
 
+function Obj2Node(obj, rootstring){
+    let result = '<'+rootstring+'>\n'
+    for(let prop of Object.keys(obj)){
+        if (obj[prop] instanceof Array){
+            result += '<'+prop+'>\n'
+            for(let item of obj[prop]) {
+                result += Obj2Node(item, 'item')
+            }
+            result += '</'+prop+'>\n'
+        } else if (obj[prop] instanceof Object){
+            result += '<'+prop+' '
+            for(let innerprop of Object.keys(obj[prop])){
+                result += innerprop + '=\'' +obj[prop][innerprop] +'\' '
+            }
+            result += '>\n'
+            result += '</'+prop+'>\n'
+        } else {
+            result += '<'+prop+'>'
+            result += obj[prop]
+            result += '</'+prop+'>\n'
+        }
+    }
+    result += '</'+rootstring+'>'
+    return result
+}
